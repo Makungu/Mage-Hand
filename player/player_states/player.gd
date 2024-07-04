ba617a436393177aba622920f22a8a65f2b14e56
Michael <113719606+CatGutsNightmare@users.noreply.github.com>
2024-07-04 15:18:25 +0200

    more pain

--- player/player_states/player.gd
+++ player/player_states/player.gd
@@ -5,6 +5,9 @@ extends CharacterBody2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@export var loca : Node2D
+@export var goca : Node2D
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -17,6 +20,8 @@ func _physics_process(delta):
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
+	
+	loca.global_position = global_position
+	#loca.position = loca.global_position 
 	move_and_slide()
 	
